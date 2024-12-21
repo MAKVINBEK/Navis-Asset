2a39acadfa87c381f59d80f631898aca9e2abdf7
MAKVINBEK <bekamametov07@gmail.com>
2024-12-21 17:41:46 +0600

    join

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,5 +1,4 @@
 import AcardionBlock from "../component/acardion/AcardionTitle"
-import Banner from "../component/banner/Banner"
 import Advantages from "../component/block_advantages/Advantages"
 import Services from "../component/block_services/Services"
 import Form from "../component/from/From"
@@ -10,7 +9,6 @@ import Partners from "../component/partners/Partners"
 const Menu = () => {
     return(
         <>
-              <Banner />
               <Partners />
               <Advantages />
               <Services />
